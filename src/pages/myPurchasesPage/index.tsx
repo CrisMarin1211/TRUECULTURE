8255618e3662a93dc369bfa635d1450d1cc1e638
anasofia01 <sofiapacheco0423@gmail.com>
2025-09-22 21:10:48 -0500

    feat: se agrega componente en el page

--- src/pages/myPurchasesPage/index.tsx
+++ src/pages/myPurchasesPage/index.tsx
@@ -1,9 +1,12 @@
+import Header from '../../components/header';
 import './style.css';
 
 const MyPurchasesPage = () => {
   return (
     <>
-      <div className="my-purchases-page"></div>
+      <div className="my-purchases-page">
+        <Header />
+      </div>
     </>
   );
 };

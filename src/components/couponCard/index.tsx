20b79de7e292e2cf35dea7004269647f5fc86466
anasofia01 <sofiapacheco0423@gmail.com>
2025-09-22 21:26:22 -0500

    feat: se hace el html del componente

---
+++ src/components/couponCard/index.tsx
@@ -0,0 +1,20 @@
+import './style.css';
+
+type CouponCardProps = {
+  title: string;
+  description: string;
+  value: string;
+};
+
+export const CouponCard = ({ title, description, value }: CouponCardProps) => {
+  return (
+    <div className="coupon-card">
+      <div className="coupon-info">
+        <h3 className="coupon-title">{title}</h3>
+        <p className="coupon-description">{description}</p>
+        <span className="coupon-value">{value}</span>
+      </div>
+      <button className="redeem-btn">Redimir Cupon</button>
+    </div>
+  );
+};

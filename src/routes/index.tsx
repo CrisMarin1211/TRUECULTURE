74b24b196da73a2601a64a09cf115eb20d4d180c
anasofia01 <sofiapacheco0423@gmail.com>
2025-09-23 18:54:25 -0500

    feat: se agrega ruta de cartPage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from '../pages/homePage';
 import MyPurchasesPage from '../pages/myPurchasesPage';
+import MyCartPage from '../pages/myCartPage';
 
 const AppRoutes = () => {
   return (
@@ -8,6 +9,7 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/my-purchases" element={<MyPurchasesPage />} />
+        <Route path="/my-cart" element={<MyCartPage />} />
       </Routes>
     </>
   );

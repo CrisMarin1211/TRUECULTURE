57db0a2d830632abf8b733e7b65bbc0c08f79039
anasofia01 <sofiapacheco0423@gmail.com>
2025-09-22 20:47:00 -0500

    feat: se modifica app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { BrowserRouter } from 'react-router-dom';
+import AppRoutes from './routes';
+
 function App() {
   return (
     <>
-      <h1></h1>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
     </>
   );
 }

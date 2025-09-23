bf7482c218406fec8123ebad97a61bd0d4ec778d
anasofia01 <sofiapacheco0423@gmail.com>
2025-09-22 21:04:02 -0500

    feat: se hace la data de coupons

---
+++ src/data/data.ts
@@ -0,0 +1,14 @@
+export const coupons = [
+  {
+    title: 'Cupón 1',
+    description: 'Aplica para tu próxima compra',
+    value: '15%',
+  },
+  {
+    title: 'Cupón 2',
+    description: 'Descuento en productos seleccionados',
+    value: '20%',
+  },
+  { title: 'Cupón 3', description: 'Oferta especial', value: '$10' },
+  { title: 'Cupón 4', description: 'Válido este mes', value: '5%' },
+];

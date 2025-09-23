fd61749a81660c9c00dea9a2f8a33435d83f3f8f
anasofia01 <sofiapacheco0423@gmail.com>
2025-09-22 21:12:06 -0500

    feat: se define ruta de mis compras

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from '../pages/homePage';
+import MyPurchasesPage from '../pages/myPurchasesPage';
 
 const AppRoutes = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/my-purchases" element={<MyPurchasesPage />} />
       </Routes>
     </>
   );

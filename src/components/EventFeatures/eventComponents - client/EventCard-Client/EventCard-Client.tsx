6884f9d94fb81c6db5b5f5e0e11939121a76d810
Geraldine Sastoque <gerisastoque3009@gmail.com>
2025-09-24 16:32:47 -0500

    Feat: creacion estructura html de compoente de eventos cliente

---
+++ src/components/EventFeatures/eventComponents - client/EventCard-Client/EventCard-Client.tsx
@@ -0,0 +1,26 @@
+const EventCardClient = () => {
+  return (
+    <>
+      <div>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <div>
+            <div>
+              <p>topa tolondra</p>
+              <p>clase salsa</p>
+            </div>
+            <div>
+              <p>Agosto 26</p>
+              <p>$10.00</p>
+            </div>
+          </div>
+          <button>ver mas detalle</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default EventCardClient;

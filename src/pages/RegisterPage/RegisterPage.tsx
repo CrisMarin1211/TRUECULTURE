da116e9b4d0b4168efd7992097c340b4350e6eb9
CrisMarin1211 <alca81411@gmail.com>
2025-09-25 03:09:27 -0500

    feat: Se agrego estructura base a la página de registro con componentes

---
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -0,0 +1,26 @@
+import './RegisterPage.css';
+import RegisterForm from '../../components/registerComponents/RegisterForm/RegisterForm';
+import LogoCompleto from '../../assets/Marca/Logocompleto.png';
+import LogoFucsia from '../../assets/Marca/LogoFucsia.png';
+
+const RegisterPage = () => {
+  return (
+    <div className="register-page">
+      <header className="header-register">
+        <img src={LogoCompleto} alt="LogoCompleto" className="logocompleto" />
+
+        <span className="phrase-register">Por favor, ingresa tus datos para crear una cuenta.</span>
+
+        <div className="register-container">
+          <RegisterForm />
+        </div>
+
+        <footer className="footer-register">
+          <img src={LogoFucsia} alt="LogoFucsia" className="logofucsia" />
+        </footer>
+      </header>
+    </div>
+  );
+};
+
+export default RegisterPage;

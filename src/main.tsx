483fa245f177d28a5f5c8ce899a153281fbff2c7
Geraldine Sastoque <gerisastoque3009@gmail.com>
2025-09-26 02:09:27 -0500

    Feat: se agrego el provider del theme en el app

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,18 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { EventProvider } from './context/EventContext.tsx';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from './styles/theme.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <EventProvider>
-      <App />
-    </EventProvider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <EventProvider>
+        <App />
+      </EventProvider>
+    </ThemeProvider>
   </StrictMode>,
 );

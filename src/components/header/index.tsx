f31e11b9d1a962a548a5bdadd6b60dea43ab5d27
Geraldine Sastoque <gerisastoque3009@gmail.com>
2025-09-26 08:56:50 -0500

    Feat: sidebar componente y css

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,44 @@
+import { useState } from 'react';
 import './style.css';
 import AvatarLetter from '../AvatarLetter/LetterAvatars';
+import SideBar from '../UiAtoms/SideBar/SideBar';
+import { styled } from '@mui/material/styles';
+
+const Overlay = styled('div')({
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  width: '100vw',
+  height: '100vh',
+  backgroundColor: 'rgba(0,0,0,0.4)', // semitransparente
+  zIndex: 1000,
+});
+
+const SidebarContainer = styled('div')({
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  height: '100vh',
+  zIndex: 1001,
+});
 
 const Header = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen((prev) => !prev);
+  };
+
   return (
     <header className="header">
       <div className="header-left">
-        <img src="/icons/menu.png" alt="Menu" className="icon menu-icon" />
+        <img
+          src="/icons/menu.png"
+          alt="Menu"
+          className="icon menu-icon"
+          onClick={toggleSidebar}
+          style={{ cursor: 'pointer' }}
+        />
       </div>
 
       <div className="header-center">
@@ -16,6 +49,16 @@ const Header = () => {
         <img src="/icons/cart.png" alt="Carrito" className="icon cart-icon" />
         <AvatarLetter />
       </div>
+
+      {/* Sidebar como modal */}
+      {isSidebarOpen && (
+        <>
+          <Overlay onClick={toggleSidebar} /> {/* cierra al hacer click afuera */}
+          <SidebarContainer>
+            <SideBar toggleSidebar={toggleSidebar} /> {/* pasamos la función */}
+          </SidebarContainer>
+        </>
+      )}
     </header>
   );
 };

cb0009dc33aab0671d3e75a4bb62ce6a1e2cd3c9
anasofia01 <sofiapacheco0423@gmail.com>
2025-09-27 08:51:30 -0500

    feat: se crea una ruta

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -20,6 +20,7 @@ const AppRoutes = () => {
         <Route path="/my-profile" element={<ProfilePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<RegisterPage />} />
+        <Route />
       </Routes>
     </>
   );

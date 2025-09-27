b1e477eb1bfe92dc6e574043873647531fbb3e69
anasofia01 <sofiapacheco0423@gmail.com>
2025-09-27 09:11:19 -0500

    fix: se corrige el type

--- src/components/createEvent/index.tsx
+++ src/components/createEvent/index.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './style.css';
 import { useNavigate, useParams } from 'react-router-dom';
 
-type Product = {
+type Event = {
   id: string;
   image: string;
   imageFile: File | null;

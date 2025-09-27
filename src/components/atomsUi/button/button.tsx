4d2f193fb9195ea5911aaa9c6c57ed8adab05f3c
CrisMarin1211 <alca81411@gmail.com>
2025-09-27 12:53:05 -0500

    fix: ajustes en componentes y estilos de staging

---
+++ src/components/atomsUi/button/button.tsx
@@ -0,0 +1,17 @@
+import './style.css';
+
+type ButtonProps = {
+  label: string;
+  onClick?: () => void;
+  type?: 'button' | 'submit';
+};
+
+const Button = ({ label, onClick, type = 'button' }: ButtonProps) => {
+  return (
+    <button type={type} onClick={onClick} className="btn">
+      {label}
+    </button>
+  );
+};
+
+export default Button;

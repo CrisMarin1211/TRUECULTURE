7e81799f4128639958ed4b4173b0e9f147b7621a
CrisMarin1211 <alca81411@gmail.com>
2025-09-27 17:23:41 -0500

    chore: agregar prueba temporal en MarketplaceHome

--- src/pages/MarketplaceHome/MarketplaceHome.tsx
+++ src/pages/MarketplaceHome/MarketplaceHome.tsx
@@ -1,10 +1,31 @@
-import MarketplaceContainer from '../../components/marketplacesComponents/MarketplaceContainer/MarketplaceContainer';
-const MarketplaceHome = () => {
+// import MarketplaceContainer from '../../components/marketplacesComponents/MarketplaceContainer/MarketplaceContainer';
+// const MarketplaceHome = () => {
+//   return (
+//     <div className="marketplace-page">
+//       <MarketplaceContainer />
+//     </div>
+//   );
+// };
+
+// export default MarketplaceHome;
+
+import Header from '../../components/header/index';
+import FeaturesProductList from '../../components/List/Featured/featuredProductList/index';
+import ProductList from '../../components/List/categories/productList/index';
+
+const TAGS = ['Afiches', 'Pines', 'Cultural', 'Moda', 'Gastronomía'];
+
+const MarketplaceHome: React.FC = () => {
   return (
-    <div className="marketplace-page">
-      <MarketplaceContainer />
-    </div>
+    <>
+      <Header />
+      <div>
+        <FeaturesProductList />
+      </div>
+      {TAGS.map((tag) => (
+        <ProductList key={tag} tag={tag as any} />
+      ))}
+    </>
   );
 };
-
 export default MarketplaceHome;

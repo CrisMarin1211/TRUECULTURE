406a63bba5cbff29195009a7a33e6057b49e98ac
anasofia01 <sofiapacheco0423@gmail.com>
2025-09-27 08:21:55 -0500

    fix: se comenta la linea 9

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from './styles/theme.tsx';
 import { CityProvider } from './context/CityContex.tsx';
-import './index.css';
+// import './index.css';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';

2c0e18bc6e15691fb1def24b65a4e684956b843d
CrisMarin1211 <alca81411@gmail.com>
2025-09-28 14:41:25 -0500

    feat: agregar ProductProvider al contexto de la aplicación

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes';
 import { AuthProvider } from './context/AuthContext';
+import { ProductProvider } from './context/ProductEvent';
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <AppRoutes />
+        <ProductProvider>
+          <AppRoutes />
+        </ProductProvider>
       </AuthProvider>
     </BrowserRouter>
   );

be565a8deb6f51d4af0214fadff392909653670e
anasofia01 <sofiapacheco0423@gmail.com>
2025-09-28 17:13:02 -0500

    feat: se modifica la page

--- src/pages/AdminPages/eventsPages/createPage/index.tsx
+++ src/pages/AdminPages/eventsPages/createPage/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './style.css';
 import CreateEvent from '../../../../components/createEvent';
+import SidebarAdmin from '../../../../components/atomsUi/sideBarAdmin';
 
 const CreateEventPage: React.FC = () => {
   return (
     <div className="page-container">
-      <aside className="sidebar"></aside>
+      <SidebarAdmin />
 
       <main className="main-content">
         <CreateEvent />

3d7b8a2a090e309622941d0258091103605a4f5d
Geraldine Sastoque <gerisastoque3009@gmail.com>
2025-09-29 09:07:42 -0500

    core> css de marjetplace

--- src/pages/MarketplaceHome/MarketplaceHome.tsx
+++ src/pages/MarketplaceHome/MarketplaceHome.tsx
@@ -1,6 +1,7 @@
 import Header from '../../components/header';
 import FeaturedProductList from '../../components/List/Featured/featuredProductList/index';
 import ProductList from '../../components/List/categories/productList/index';
+import { Box } from '@mui/material';
 
 const TAGS = ['Afiches', 'Pines', 'Cultural', 'Moda', 'Gastronomía'];
 
@@ -8,12 +9,17 @@ const MarketplaceHome: React.FC = () => {
   return (
     <>
       <Header />
-      <div>
-        <FeaturedProductList />
-      </div>
-      {TAGS.map((tag) => (
-        <ProductList key={tag} tag={tag as any} />
-      ))}
+      <Box sx={{ px: 2, mt: 5 }}>
+        <Box sx={{ mb: 10 }}>
+          <FeaturedProductList />
+        </Box>
+
+        {TAGS.map((tag) => (
+          <Box key={tag} sx={{ mb: 10 }}>
+            <ProductList tag={tag as any} />
+          </Box>
+        ))}
+      </Box>
     </>
   );
 };

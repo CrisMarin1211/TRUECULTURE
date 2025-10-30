c131bd271463b36347eaf349e9e92ccb6b9eb969
anasofia01 <sofiapacheco0423@gmail.com>
2025-10-29 19:15:24 -0500

    feat: se define el tipo de ticket

---
+++ src/types/TicketType.tsx
@@ -0,0 +1,10 @@
+export interface TicketItem {
+  id?: number;
+  customer_name: string;
+  order_number: string;
+  ticket_type: 'General' | 'VIP' | 'Backstage';
+  quantity: number;
+  payment_status: 'Pagado' | 'Pendiente' | 'Fallido';
+  event_id: number;
+  date: string;
+}

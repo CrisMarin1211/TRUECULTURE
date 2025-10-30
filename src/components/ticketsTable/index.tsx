74d844c9f7cf65e0dea541e256b41276856754a0
anasofia01 <sofiapacheco0423@gmail.com>
2025-10-29 19:13:58 -0500

    fix: se corrige error

--- src/components/ticketsTable/index.tsx
+++ src/components/ticketsTable/index.tsx
@@ -30,7 +30,7 @@ const TicketsTable: React.FC<TicketsTableProps> = ({ tickets }) => {
               <td>{ticket.order_number}</td>
               <td>{ticket.ticket_type}</td>
               <td>{ticket.quantity}</td>
-              <td className={status ${ticket.payment_status.toLowerCase()}}>
+              <td className={`status ${ticket.payment_status.toLowerCase()}`}>
                 {ticket.payment_status}
               </td>
             </tr>

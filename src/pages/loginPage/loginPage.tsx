23b11a3910d2b2007251187cb92cd20786403bb7
anasofia01 <sofiapacheco0423@gmail.com>
2025-10-29 19:18:40 -0500

    feat: se ajusta el login con el admin

--- src/pages/loginPage/loginPage.tsx
+++ src/pages/loginPage/loginPage.tsx
@@ -1,16 +1,20 @@
+import { useLocation } from 'react-router-dom';
 import './loginPage.css';
 import LoginForm from '../../components/loginComponents/LoginForm/LoginForm';
 import LogoCompleto from '../../assets/Marca/Logocompleto.png';
 import Logoazul from '../../assets/Marca/logoazul.png';
 
 const LoginPage = () => {
+  const location = useLocation();
+  const fromAdmin = location.state?.fromAdmin || false;
+
   return (
     <div className="login-page">
       <header className="header-login">
         <img src={LogoCompleto} alt="LogoCompleto" className="logocompleto" />
         <span className="phrase">Estamos felices de tenerte de vuelta.</span>
         <div className="login-container">
-          <LoginForm />
+          <LoginForm fromAdmin={fromAdmin} />
         </div>
       </header>
 

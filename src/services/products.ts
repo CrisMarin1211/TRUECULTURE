24518a4a4bc83f6bd0006d1efc6001b9046a8c56
anasofia01 <sofiapacheco0423@gmail.com>
2025-11-03 09:36:23 -0500

    feat: se modifica el service de products

--- src/services/products.ts
+++ src/services/products.ts
@@ -7,6 +7,16 @@ export const getProducts = async (): Promise<ProductItem[]> => {
   return data ?? [];
 };
 
+export const getProductsByOrganization = async (organization?: string) => {
+  const { data, error } = await supabase
+    .from('products')
+    .select('*')
+    .eq('organization', organization);
+
+  if (error) throw error;
+  return data;
+};
+
 export const addProduct = async (product: Omit<ProductItem, 'id'>): Promise<ProductItem[]> => {
   const { data, error } = await supabase.from('products').insert([product]).select('*');
   if (error) throw error;
@@ -28,12 +38,16 @@ export const deleteProduct = async (id: number): Promise<ProductItem[]> => {
   return data ?? [];
 };
 
-export const getTotalProducts = async (): Promise<number> => {
+export const getTotalProducts = async (organization: string): Promise<number> => {
   const { count, error } = await supabase
     .from('products')
-    .select('*', { count: 'exact', head: true });
+    .select('*', { count: 'exact', head: true })
+    .eq('organization', organization);
 
-  if (error) throw error;
+  if (error) {
+    console.error('Error al obtener total de productos:', error);
+    return 0;
+  }
 
-  return count || 0;
+  return count ?? 0;
 };

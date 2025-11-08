b853154439ceaa90184a7ee3931f7e27d0cf1ab3
anasofia01 <sofiapacheco0423@gmail.com>
2025-11-07 21:24:02 -0500

    feat: se crea nuevo tipo orderItemsType

---
+++ src/types/OrderItemsType.tsx
@@ -0,0 +1,12 @@
+export interface EventActivity {
+  eventLetter: string;
+  eventName: string;
+  participants: number;
+  percent: number;
+}
+
+export interface EventSales {
+  itemName: string;
+  totalQuantity: number;
+  totalPrice: number;
+}

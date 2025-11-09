a7ffaebfe83e8900dd581cdd6adb87d6696c8c8c
CrisMarin1211 <alca81411@gmail.com>
2025-11-09 01:12:23 -0500

    refactor: Se actualizó AppRoutes.tsx envolviendo las rutas protegidas con PrivateRoute.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -21,41 +21,166 @@ import ListTicketsPage from '../pages/AdminPages/ticketsPages/listPage';
 import AdminHomePage from '../pages/AdminPages/homePage';
 import AdminAnalyticsPage from '../pages/AdminPages/analyticPage';
 
+import PrivateRoute from './PrivateRoute';
+
 const AppRoutes = () => {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/my-purchases" element={<MyPurchasesPage />} />
-        <Route path="/my-cart" element={<MyCartPage />} />
-        <Route path="/my-coupons" element={<MyCouponsPage />} />
-        <Route path="/my-profile" element={<ProfilePage />} />
+    <Routes>
+      {/* Públicas */}
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<RegisterPage />} />
+      <Route path="/Marketplacehome" element={<MarketplaceHome />} />
+      <Route path="/categories/:city/:tag" element={<Categories />} />
 
-        <Route path="/dashboard" element={<AdminHomePage />} />
+      {/* Cliente */}
+      <Route
+        path="/DashboardClient"
+        element={
+          <PrivateRoute>
+            <DashboardClient />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/my-purchases"
+        element={
+          <PrivateRoute>
+            <MyPurchasesPage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/my-cart"
+        element={
+          <PrivateRoute>
+            <MyCartPage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/my-coupons"
+        element={
+          <PrivateRoute>
+            <MyCouponsPage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/my-profile"
+        element={
+          <PrivateRoute>
+            <ProfilePage />
+          </PrivateRoute>
+        }
+      />
 
-        <Route path="/create-product" element={<CreateProductPage />} />
-        <Route path="/edit-product/:id" element={<CreateProductPage />} />
-        <Route path="/list-products" element={<ListProductPage />} />
-
-        <Route path="/create-event" element={<CreateEventPage />} />
-        <Route path="/edit-event/:id" element={<CreateEventPage />} />
-        <Route path="/list-events" element={<ListEventPage />} />
-
-        <Route path="/create-ticket" element={<CreateTicketPage />} />
-        <Route path="/edit-ticket/:id" element={<CreateTicketPage />} />
-        <Route path="/list-tickets" element={<ListTicketsPage />} />
-
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<RegisterPage />} />
-
-        <Route path="/DashboardClient" element={<DashboardClient />} />
-        <Route path="/Marketplacehome" element={<MarketplaceHome />} />
-        <Route path="/categories/:city/:tag" element={<Categories />} />
-        <Route path="/settings" element={<ProfileAdminPage />} />
-        <Route path="/list-comments" element={<ListCommentsPage />} />
-        <Route path="/analytics" element={<AdminAnalyticsPage />} />
-      </Routes>
-    </>
+      {/* Admin */}
+      <Route
+        path="/dashboardAdmin"
+        element={
+          <PrivateRoute>
+            <AdminHomePage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/create-product"
+        element={
+          <PrivateRoute>
+            <CreateProductPage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/edit-product/:id"
+        element={
+          <PrivateRoute>
+            <CreateProductPage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/list-products"
+        element={
+          <PrivateRoute>
+            <ListProductPage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/create-event"
+        element={
+          <PrivateRoute>
+            <CreateEventPage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/edit-event/:id"
+        element={
+          <PrivateRoute>
+            <CreateEventPage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/list-events"
+        element={
+          <PrivateRoute>
+            <ListEventPage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/create-ticket"
+        element={
+          <PrivateRoute>
+            <CreateTicketPage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/edit-ticket/:id"
+        element={
+          <PrivateRoute>
+            <CreateTicketPage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/list-tickets"
+        element={
+          <PrivateRoute>
+            <ListTicketsPage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/settings"
+        element={
+          <PrivateRoute>
+            <ProfileAdminPage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/list-comments"
+        element={
+          <PrivateRoute>
+            <ListCommentsPage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/analytics"
+        element={
+          <PrivateRoute>
+            <AdminAnalyticsPage />
+          </PrivateRoute>
+        }
+      />
+    </Routes>
   );
 };
 
